97e2a0507e69d9376a3c3c0553ff0d21a41a3b86
CyberBryan <beanious@gmail.com>
2016-12-27 10:50:21 -0800

    Updated
    Updated for appending

--- sysmon_parse.cmd
+++ sysmon_parse.cmd
@@ -36,7 +36,7 @@ REM Setup:
 REM Set base folder, set input source file if needed, and make Results directory.
 SET scriptlocation=%~dp0
 SET src=%1
-SET dtstamp=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
+SET dtstamp=%date:~-4%%date:~4,2%%date:~7,2%
 mkdir Results_%dtstamp%
 IF EXIST %src% GOTO parse
 REM
@@ -52,7 +52,7 @@ CLS
 REM Parse Sysmon Event Log using Microsoft's Logparser
 REM http://www.microsoft.com/en-us/download/details.aspx?id=24659
 ECHO Sysmon Log Copied, Now Parsing
-tools\logparser\logparser -i:evt -o:csv "Select RecordNumber,TO_UTCTIME(TimeGenerated),EventID,SourceName,ComputerName,SID,Strings from %src% WHERE EventID in ('1';'2';'3';'4';'5';'6';'7';'8')" > Results_%dtstamp%\sysmon_parsed.txt
+tools\logparser\logparser -i:evt -o:csv "Select RecordNumber,TO_UTCTIME(TimeGenerated),EventID,SourceName,ComputerName,SID,Strings from %src% WHERE EventID in ('1';'2';'3';'4';'5';'6';'7';'8')" >> Results_%dtstamp%\sysmon_parsed.txt
 REM
 REM ------------------------------------------------------------------------------------------------------------------------
 REM Remove copied Sysmon Event Log as it is no longer needed.
@@ -63,16 +63,16 @@ REM Extract Information from text file using TekDefense's tekcollect.py tool.
 REM http://www.tekdefense.com/tekcollect/
 ECHO ..Log Parsed, Now Extracting Data
 cd tools\tekcollect
-python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t MD5 > %scriptlocation%Results_%dtstamp%\MD5Hashes.txt
-python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t SHA1 > %scriptlocation%Results_%dtstamp%\Sha1Hashes.txt
-python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t SHA256 > %scriptlocation%Results_%dtstamp%\SHA256Hashes.txt
-python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t domain > %scriptlocation%Results_%dtstamp%\Domains.txt
-python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t url > %scriptlocation%Results_%dtstamp%\URL.txt
+python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t MD5 >> %scriptlocation%Results_%dtstamp%\MD5Hashes.txt
+python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t SHA1 >> %scriptlocation%Results_%dtstamp%\Sha1Hashes.txt
+python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t SHA256 >> %scriptlocation%Results_%dtstamp%\SHA256Hashes.txt
+python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t domain >> %scriptlocation%Results_%dtstamp%\Domains.txt
+python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t url >> %scriptlocation%Results_%dtstamp%\URL.txt
 ECHO begin > %scriptlocation%Results_%dtstamp%\IPs.txt
 ECHO verbose >> %scriptlocation%Results_%dtstamp%\IPs.txt
 python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t ip4 >> %scriptlocation%Results_%dtstamp%\IPs.txt
 ECHO end >> %scriptlocation%Results_%dtstamp%\IPs.txt
-python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t exe > %scriptlocation%Results_%dtstamp%\Executables.txt
+python tekcollect.py -f %scriptlocation%Results_%dtstamp%\sysmon_parsed.txt -t exe >> %scriptlocation%Results_%dtstamp%\Executables.txt
 REM
 REM ------------------------------------------------------------------------------------------------------------------------
 REM This section uses a list of terms contained in "sysmon_keywords.txt" to search the parsed Sysmon results 
@@ -89,7 +89,7 @@ REM This section left in script for those who want to send lookups through Cymru
 REM https://www.team-cymru.org/Services/ip-to-asn.html#whois
 REM Results are saved to delimited file and can be sorted by Country, Owner Name, Resolved Name, etc.
 REM cd %scriptlocation%tools\nc
-REM ncat.exe whois.cymru.com 43 < %scriptlocation%Results_%dtstamp%\IPs.txt > %scriptlocation%Results_%dtstamp%\Whois_Results.txt
+REM ncat.exe whois.cymru.com 43 < %scriptlocation%Results_%dtstamp%\IPs.txt >> %scriptlocation%Results_%dtstamp%\Whois_Results.txt
 REM
 REM ------------------------------------------------------------------------------------------------------------------------
 REM Using NirSoft's IPNetInfo GUI Tool for bulk Whois
